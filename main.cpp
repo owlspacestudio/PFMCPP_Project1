512277b21f47febab7d70c929794194dc39e649a
owlspacestudio <owlspacestudio@me.com>
2020-01-13 23:31:07 +0000

    completed Project 1 task

--- main.cpp
+++ main.cpp
@@ -32,88 +32,122 @@ arm.rotateCounterClockwise(); //demonstrates CamelCase
 
 /*
  1)
- Noun:
- action 1:
- action 2:
- action 3:
+ Noun:        cat
+ action 1:    the cat walks
+ action 2:    the cat drinks
+ action 3:    the cat chases a mouse
  */
+ cat.walk();
+ cat.drink();
+ cat.chaseMouse();
 
 
 /*
  2)
- Noun:
- action 1:
- action 2:
- action 3:
+ Noun:        pitch
+ action 1:    the pitch goes up a semitone 
+ action 2:    the pitch goes down a semitone
+ action 3:    the pitch goes up an octave
  */
+ pitch.upSemitone();
+ pitch.downSemitone();
+ pitch.upOctave();
+
+
 
 /*
  3)
- Noun:
- action 1:
- action 2:
- action 3:
+ Noun:        head
+ action 1:    the head nods
+ action 2:    the head turns left
+ action 3:    the head turns right
  */
+ head.nod();
+ head.turnLeft();
+ head.turnRight();
 
 
 /*
  4)
- Noun:
- action 1:
- action 2:
- action 3:
+ Noun:        car
+ action 1:    the car starts
+ action 2:    the car stops
+ action 3:    the car accelerates
  */
+ car.start();
+ car.stop();
+ car.accelerate();
 
 /*
  5)
- Noun:
- action 1:
- action 2:
- action 3:
+ Noun:        baby
+ action 1:    the baby sleeps
+ action 2:    the baby cries
+ action 3:    the baby smiles
  */
+ baby.sleep();
+ baby.cry();
+ baby.smile();
+
 
 
 /*
  6)
- Noun:
- action 1:
- action 2:
- action 3:
+ Noun:        water
+ action 1:    the water pours
+ action 2:    the water drips
+ action 3:    the water runs into the bath
  */
+ water.pour();
+ water.drip();
+ water.runIntoSink();
+
 
 /*
  7)
- Noun:
- action 1:
- action 2:
- action 3:
+ Noun:        snow
+ action 1:    the snow melts
+ action 2:    the snow falls
+ action 3:    the snow covers the ground
  */
+ snow.melt();
+ snow.fall();
+ snow.coverGround();
 
 
 /*
  8)
- Noun:
- action 1:
- action 2:
- action 3:
+ Noun:        man 
+ action 1:    the man smokes 
+ action 2:    the man lies down
+ action 3:    the man eats lunch
  */
+ man.smoke();
+ man.lieDown();
+ man.eatLunch();
 
 /*
  9)
- Noun:
- action 1:
- action 2:
- action 3:
+ Noun:        music 
+ action 1:    the music gets louder
+ action 2:    the music gets softer
+ action 3:    the music stops
  */
+ music.getLouder();
+ music.getSofter();
+ music.stop();
 
 
 /*
  10)
- Noun:
- action 1:
- action 2:
- action 3:
+ Noun:        moon  
+ action 1:    the moon rises
+ action 2:    the moon gets brighter
+ action 3:    the moon is obscured by clouds
  */
+ moon.rise();
+ moon.getBrighter();
+ moon.obscuredCloud();
 
 
 #endif
